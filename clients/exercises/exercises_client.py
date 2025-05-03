46db32af3d3a79f1a684fd6a6a5529b5999e097b
FattakhovNikita <hiki4roll@gmail.com>
2025-05-03 22:37:16 +0500

    Добавление Allure шагов в автотесты. Практика работы с allure.step

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -5,13 +5,14 @@ from clients.private_http_builder import AuthenticationUserSchema, get_private_h
 from clients.exercises.exercises_schema import GetExerciseResponseSchema, \
     UpdateExerciseResponseSchema, UpdateExerciseRequestSchema, CreateExerciseResponseSchema, \
     CreateExerciseRequestSchema, GetExercisesQuerySchema, GetExercisesResponseSchema
-
+import allure
 
 class ExercisesClient(APIClient):
     """
     Клиент для работы с /api/v1/exercises
     """
 
+    @allure.step("Get exercises")
     def get_exercises_api(self, query: GetExercisesQuerySchema) -> Response:
         """
         Метод получения списка заданий для определенного курса по идентификатору.
@@ -21,6 +22,7 @@ class ExercisesClient(APIClient):
         """
         return self.get("/api/v1/exercises", params=query.model_dump(by_alias=True))
 
+    @allure.step("Get exercise by id {exercise_id}")
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод получения информации о задании по UUID.
@@ -30,6 +32,7 @@ class ExercisesClient(APIClient):
         """
         return self.get(f"/api/v1/exercises/{exercise_id}")
 
+    @allure.step("Create exercise")
     def create_exercise_api(self, request: CreateExerciseRequestSchema) -> Response:
         """
         Метод выполняет создание нового задания.
@@ -39,6 +42,7 @@ class ExercisesClient(APIClient):
         """
         return self.post("/api/v1/exercises", json=request.model_dump(by_alias=True))
 
+    @allure.step("Update exercise by id {exercise_id}")
     def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestSchema) -> Response:
         """
         Метод обновления задания по идентификатору.
@@ -49,6 +53,7 @@ class ExercisesClient(APIClient):
         """
         return self.patch(f"/api/v1/exercises/{exercise_id}", json=request.model_dump(by_alias=True))
 
+    @allure.step("Delete exercise by id {exercise_id}")
     def delete_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод удаления задания по идентификатору.
